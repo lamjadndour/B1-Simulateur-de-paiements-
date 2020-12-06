b1c5fd9f53661fc1fac4f3e78bee608d5ab422a7
lamjadndour@gmail.com <lamjadndour@gmail.com>
2020-12-06 16:22:31 +0100

    add comment

--- Brief_1.java
+++ Brief_1.java
@@ -1,10 +1,9 @@
 package ex1;
 import java.util.Scanner;
-
 public class Brief_1 {
-	
+	//variable qui vas scanner le input du user
 	static Scanner input = new Scanner(System.in);
-
+	//fonction qui permet de prendre le nom complet et un message d’accueil 
 	static String welcome() {
 		String text1 = "Quel est votre nom";
 		String text2 = "Quel est votre prénom";
@@ -17,29 +16,30 @@ public class Brief_1 {
 		System.out.println("Bonjour " + name);
 		return name;
 	}
+	//function qui permet de simuler le calcul des indemnisations
 	static void travaille(String name) {
 		double h = 0;
 		double t = 0;
 		double payer = 0;
 		double err = 1;
-		
 		System.out.println("veuillez introduire le nombre d’heures travaillées cette semain");
 		h = input.nextInt();
 		while(err == 1) {
-			if(h <= 60) {
+			if(h <= 60 && h > 0) {
 				System.out.println("veuillez introduire le tarif par heure");
 				t = input.nextInt();
-				err = 0;
-			}else if(h > 69) {
+				while(err == 1) {
+					if(t > 0) err = 0;
+					else {
+						System.out.println("!! information erronés !!\nveuillez introduire le tarif par heure");
+						t = input.nextInt();
+					}
+				}		
+			}else{
 				System.out.println("information erronés");
 				System.out.println("veuillez introduire le nombre d’heures travaillées cette semain");
 				h = input.nextInt();
 			}
-			     else {
-			    	System.out.println("number between 60 and 70 isn't consider");
-					System.out.println("veuillez introduire le nombre d’heures travaillées cette semain");
-					h = input.nextInt();
-			     }
 		}
 		if(h <= 40 ) {
 			payer = t * h;
@@ -54,17 +54,10 @@ public class Brief_1 {
 			System.out.println("prix des heurs fix(les 40 heurs) = "+payer+" MAD");
 			System.out.println("prime des heurs extra= "+(h * t)+" MAD");
 			payer = payer + (h * t);
-		}
-		
+		}	
 		System.out.println("votre indemnisation est de " + payer + " MAD \n!! programme shutdown !!");
+		}	
+		public static void main (String[]args) {	
+			travaille(welcome());
+		}
 	}
-	
-	
-	public static void main (String[] args) {
-		
-		
-		travaille(welcome());
-		
-	}
-	
-}
